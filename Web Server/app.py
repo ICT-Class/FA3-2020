2d70478331c479c6c47357b753093135719ae22e
Nick Wong <nickwong680@gmail.com>
2020-07-31 15:12:28 +1000

    add poke_info.html

--- Web Server/app.py
+++ Web Server/app.py
@@ -11,6 +11,12 @@ def index():
     # print(response.json())
     return render_template("index.html", results=response.json())
 
+@app.route('/firstpokemon')
+def firstpokemon():
+    response = requests.get('http://127.0.0.1:5000/1')
+    # print(response.json())
+    return render_template("poke_info.html", pokemon=response.json()[0])
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
