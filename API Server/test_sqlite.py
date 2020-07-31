dd7ffb919ab2af675391c4ad37659a1325e73e39
Nick Wong <nickwong680@gmail.com>
2020-07-31 10:28:22 +1000

    add get_poke_by_id() in API Server

--- API Server/test_sqlite.py
+++ API Server/test_sqlite.py
@@ -3,17 +3,28 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('pokemon.db')
-conn.row_factory = sqlite3.Row
-c = conn.cursor()
-results = c.execute("SELECT * FROM pokemon")
-pokemon_info = []
-for row in results:
-    pokemon_info.append(dict(row))
 
 
 @app.route('/')
 def pokemon():
+    conn = sqlite3.connect('pokemon.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    results = c.execute("SELECT * FROM pokemon")
+    pokemon_info = []
+    for row in results:
+        pokemon_info.append(dict(row))
+    return jsonify(pokemon_info)
+
+@app.route("/<int:id>")
+def get_poke_by_id(id):
+    conn = sqlite3.connect('pokemon.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    results = c.execute("SELECT * FROM pokemon WHERE pokedexID =" + str(id))
+    pokemon_info = []
+    for row in results:
+        pokemon_info.append(dict(row))
     return jsonify(pokemon_info)
 
 
