83177410b9d1e8687b88c0c604b8df795b9a9c53
Nick <nickwong680@gmail.com>
2020-08-26 06:43:06 +1000

    add add.html page for adding pokemon

--- Web Server/app.py
+++ Web Server/app.py
@@ -11,11 +11,30 @@ def index():
     return render_template("index.html", caught=caught.json())
 
 
-@app.route('/firstpokemon')
+@app.route('/pokemon')
 def firstpokemon():
-    response = requests.get('http://127.0.0.1:5000/22')
+    response = requests.get('http://127.0.0.1:5000/pokemon')
     # print(response.json())
-    return render_template("poke_info.html", pokemon=response.json()[0])
+    return render_template("pokemon.html", pokemons=response.json())
+
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
+
+
+@app.route('/add', methods=['POST'])
+def response():
+    newPoke = {
+        "name": request.form.get("name"),
+        "hp": request.form.get("hp"),
+        "attack": request.form.get("attack"),
+        "defense": request.form.get("defense"),
+        "speed": request.form.get("speed"),
+        "image": request.form.get("image"),
+    }
+    requests.post('http://127.0.0.1:5000/pokemon/add', json=newPoke)
+    return render_template("add.html", name=newPoke['name'], hp=newPoke["hp"], attack=newPoke["attack"], defense=newPoke["defense"], speed=newPoke['speed'], image=newPoke['image'])
 
 
 if __name__ == '__main__':
